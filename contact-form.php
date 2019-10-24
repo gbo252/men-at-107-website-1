c5b9753a4384619ccec29a3dc4b83e64efe1275a
gbo252 <guy.owen@virginmedia.com>
2019-10-24 22:47:02 +0100

    contact form fix

--- contact-form.php
+++ contact-form.php
@@ -4,8 +4,6 @@
   $message = $_POST['message'];
   $website = $_POST['website'];
 
-  if (!empty($website)) die();
-
   $email_from = 'info@menat107.co.uk';
 
   $email_subject = "Website Form Submission";
@@ -20,8 +18,8 @@
 
   $headers .= "Reply-To: $visitor_email \r\n";
 
-  if ($name == "" || $visitor_email == "" || $message == "" || $name == " " || $visitor_email == " " || $message == " ") {
-    die();
+  if (!empty($website) || $name == "" || $visitor_email == "" || $message == "" || $name == " " || $visitor_email == " " || $message == " ") {
+    header("Location: contact.html");
   } else {
     mail($to,$email_subject,$email_body,$headers);
   }
